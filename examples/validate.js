40fb4220e8682ec427982a52918a33611514dd36
GilbertSun <szb4321@gmail.com>
2014-07-30 14:16:48 +0800

    add struc for validate

---
+++ examples/validate.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Examples for Validate</title>
+    <script src="../sea-modules/seajs/2.3.0/dist/sea.js"></script>
+</head>
+<body>
+    <h1>This is a demo of Validate</h1>
+
+</body>
+</html>
