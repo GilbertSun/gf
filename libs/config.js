8b79fbc13c9aa4edaacc5ce9e988303a3424817c
GilbertSun <szb4321@gmail.com>
2014-07-30 14:50:35 +0800

    use requirejs to load module

---
+++ libs/config.js
@@ -0,0 +1,10 @@
+/*globals requirejs: true*/
+requirejs.config({
+    baseUrl: '../',
+    paths: {
+        depend: 'libs',
+        jquery: 'libs/jquery/jquery',
+
+        module: 'src'
+    }
+});
